fb761e2ed77cbc5df5ba331924a5ff2312253500
antoshNikolak <antoninikolak@gmail.com>
2024-01-02 14:26:01 +0000

    mice are slowed when hit by projectile

---
+++ Assets/Scripts/SlownessHandler.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+public class SlownessHandler : MonoBehaviour
+{
+    private bool isSlowed;
+    private SpriteMove spriteMove;
+    private Coroutine speedRestoreCoroutine;
+
+
+    private void Start()
+    {
+        spriteMove = GetComponent<SpriteMove>();
+    }
+
+    /// <summary> reduced speed of mouse </summary>
+    /// <param name = "slownessProjectile"> stores slowness properties of projectile </param>
+    /// <remarks> Maintained by: Antosh</remarks>
+    private void SlowMouse(SlownessProjectile slownessProjectile)
+    {
+        if (isSlowed) //stop chef being doubly slowed
+        {
+            //restart co routine so that the duration of slowness is renewed
+            StopCoroutine(speedRestoreCoroutine);
+            speedRestoreCoroutine = StartCoroutine(RestoreSpeed(slownessProjectile));
+        }
+        else
+        {
+            isSlowed = true;
+            spriteMove.moveSpeed /= slownessProjectile.slownessFactor;
+            speedRestoreCoroutine = StartCoroutine(RestoreSpeed(slownessProjectile)); //runs with delay
+        }
+    }
+
+    /// <summary> restores speed back to normal after delay </summary>
+    /// <param name = "slownessProjectile"> stores slowness properties of projectile </param>
+    /// <remarks> Maintained by: Antosh</remarks>
+    private IEnumerator RestoreSpeed(SlownessProjectile slownessProjectile)
+    {
+        yield return new WaitForSeconds(slownessProjectile.duration);
+        spriteMove.moveSpeed *= slownessProjectile.slownessFactor;
+        isSlowed = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        GameObject otherGameObject = other.gameObject;
+
+        SlownessProjectile slownessProjectile = otherGameObject.GetComponent<SlownessProjectile>();
+        if (slownessProjectile != null)
+        {
+            SlowMouse(slownessProjectile);
+        }
+    }
+}
--- Assets/Scripts/SpriteMove.cs
+++ Assets/Scripts/SpriteMove.cs
@@ -1,45 +1,54 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
 public class SpriteMove : MonoBehaviour
 {
-    [SerializeField] public float moveSpeed;
+    [SerializeField] public float moveSpeed = 2;
     private Transform[] targets;
     private Transform target;
     private int index = 0;
 
     public float totalDistanceMoved;
-    private float mouseDamage= 10; //The amount of damage that particular mouse causes to the player
+    private float mouseDamage = 10; //The amount of damage that particular mouse causes to the player
     public HealthManager health;
 
-    
+
 
     void Start()
     {
-        health= GameObject.FindGameObjectWithTag("Health").GetComponent<HealthManager>(); //finds the health manager
-        moveSpeed = 10;
+        health = GameObject.FindGameObjectWithTag("Health").GetComponent<HealthManager>(); //finds the health manager
         targets = LevelManager.LM.TurningPoints;
         target = targets[index];
-        
     }
 
     void Update()
-    {   
-        if (Vector2.Distance(target.position, transform.position) <= 0.1f){
+    {
+        if (Vector2.Distance(target.position, transform.position) <= 0.1f)
+        {
             index++;
-            if (index==targets.Length){
+            if (index == targets.Length)
+            {
                 Destroy(gameObject);
-                health.TakeDamage(mouseDamage);//the player taking damage as the mouse reaches the end
-            }else{
+                health.TakeDamage(mouseDamage); //the player taking damage as the mouse reaches the end
+            }
+            else
+            {
                 target = targets[index];
             }
         }
     }
 
-    void FixedUpdate(){
+    void FixedUpdate()
+    {
         var direction = Vector3.Normalize(target.position - transform.position);
         var movement = direction * moveSpeed * Time.deltaTime;
         transform.position += movement;
         totalDistanceMoved += movement.magnitude;
     }
+
+
+
 }
