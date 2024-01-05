dd99cb19207fcf18d39aa7737c00316769795a19
brixt01 <brixt01@outlook.com>
2024-01-05 22:47:17 +0100

    [feat] Added base ScriptableObjects for each mouse type

---
+++ Assets/ScriptableObjects/Mice.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Mouse_", menuName = "ScriptableObjects/MiceScriptableObject", order = 1)]
+public class MiceScriptableObject : ScriptableObject
+{
+    public string mouseName;
+    public float speed;
+    public float health;
+    public float size;
+    public Sprite sprite;
+    public bool canGhost;
+}
