4b0bcfd61b767d6fcac284927af83bf1c5eb1090
Martin Chien <martindog0527@gmail.com>
2024-01-12 16:42:46 +0000

    [docs]: added method comments for CreditManager.

--- Assets/Scripts/CreditManager.cs
+++ Assets/Scripts/CreditManager.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+/// <summary>
+/// Manages the credits spending in the game.
+/// </summary>
 public class CreditManager : MonoBehaviour
 {
     private int credits;
@@ -21,12 +24,21 @@ public class CreditManager : MonoBehaviour
         
     }
 
+    /// <summary>
+    /// Increases the credit amount.
+    /// </summary>
+    /// <param name="amount">The amount to increase credits by.</param>
     public void IncreaseMoney(int amount)
     {
         credits += amount;
         text.text = "Credits: " + credits;
     }
 
+    /// <summary>
+    /// Decreases the credit amount.
+    /// </summary>
+    /// <param name="amount">The amount to decrease credits by.</param>
+    /// <returns>True if credits decreases, false otherwise</returns>
     public bool SpendCredits(int amount)
     {
         if (credits >= amount)
