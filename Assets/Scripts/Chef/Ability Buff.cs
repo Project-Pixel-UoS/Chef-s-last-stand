30e883ecded700ea4593c3541c0827b7f226e217
Lishan <lisa_xulishan@126.com>
2024-03-14 02:47:51 +0000

    [feat]: add head chef to sidebar. add ability buff

---
+++ Assets/Scripts/Chef/Ability Buff.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbilityBuff : MonoBehaviour
+{
+    Collider2D[] colliders;
+    public float ATK = 0;
+    public float ATKSpeed= 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        colliders = Physics2D.OverlapCircleAll(transform.position, 3);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        colliders = Physics2D.OverlapCircleAll(transform.position, 3);
+        foreach(Collider2D collider in colliders){
+            GameObject temp = collider.gameObject;
+        }
+    }
+
+    // private void OnCollisionEnter2D(Collision2D collision){
+    //     Debug.Log("1");
+    // }
+
+    // private void OnCollisionStay2D(Collision2D collision){
+    //     Debug.Log("2");
+    // }
+
+    // private void OnCollisionExist2D(Collision2D collision){
+    //     Debug.Log("3");
+    // }
+}
