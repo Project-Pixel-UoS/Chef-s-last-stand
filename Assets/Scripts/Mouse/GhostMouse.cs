5c1d9ab31a3836b9b581ebd458bb4677574605fe
antoshNikolak <antoninikolak@gmail.com>
2024-07-18 18:10:04 +0100

    [feat] attempted to add fade ins - broken

--- Assets/Scripts/Mouse/GhostMouse.cs
+++ Assets/Scripts/Mouse/GhostMouse.cs
@@ -5,7 +5,6 @@ using Range = Chef.Range;
 
 namespace Mouse
 {
-    
     /// <summary>
     /// Responsible for showing and hiding the ghost mouse
     /// </summary>
@@ -16,44 +15,78 @@ namespace Mouse
         public bool Visible { get; private set; }
 
         private IEnumerator fadeOutCoroutine;
- 
+        private IEnumerator fadeInCoroutine;
+
+
         private void Start()
         {
             spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+            fadeInCoroutine = null;
             fadeOutCoroutine = StartCoroutineWithDelay(1, FadeOut());
             StartCoroutine(fadeOutCoroutine);
         }
 
         private void Update()
         {
-            if (IsInRangeOfHeadChef())
+            if (IsInRangeOfHeadChef() && fadeInCoroutine == null && !Visible) // check fade in not already happening
             {
-                ShowMouse();
+                print("Pre Show Mouse: " + GetAlpha());
+                // ShowMouse();
             }
-            else if (fadeOutCoroutine == null) // check fade out is not already happening
+            else if (!IsInRangeOfHeadChef() && fadeOutCoroutine == null && Visible) // check fade out is not already happening
             {
+                print("Pre hide");
                 HideMouse();
+                print("post hide alpha: " + GetAlpha());
             }
         }
 
         private void HideMouse()
         {
-            print("HIDING MOUSE");
+            if (fadeInCoroutine != null) //check currently fading
+            {
+                // interrupt fade in
+                StopCoroutine(fadeInCoroutine);
+                fadeInCoroutine = null;
+            }
+
             fadeOutCoroutine = FadeOut();
             StartCoroutine(fadeOutCoroutine);
         }
 
         private void ShowMouse()
         {
+            print("alpha 1: " + GetAlpha());
             if (fadeOutCoroutine != null) //check currently fading
             {
+                print("HERE");
                 // interrupt fade out
                 StopCoroutine(fadeOutCoroutine);
                 fadeOutCoroutine = null;
             }
-            //instantly show mouse
+
+            print("alpha 2: " + GetAlpha());
+
+            fadeInCoroutine = FadeIn();
+            StartCoroutine(fadeInCoroutine);
+        }
+
+        private IEnumerator FadeIn()
+        {
+            print("alpha 3: " + GetAlpha());
+
             Visible = true;
-            ChangeAlpha(1);
+            while (GetAlpha() < 1)
+            {
+                float n = (GetAlpha() + 0.01f);
+                ChangeAlpha(GetAlpha() + 0.01f);
+                print("alpha 4: " + GetAlpha());
+
+                yield return new WaitForSeconds(0.01f);
+            }
+
+            fadeInCoroutine = null;
+            print("complete");
         }
 
         private IEnumerator FadeOut()
@@ -63,6 +96,7 @@ namespace Mouse
                 ChangeAlpha(GetAlpha() - 0.01f);
                 yield return new WaitForSeconds(0.02f);
             }
+
             Visible = false;
             fadeOutCoroutine = null;
         }
@@ -76,8 +110,10 @@ namespace Mouse
                     return true;
                 }
             }
+
             return false;
         }
+
         public void ChangeAlpha(float alpha)
         {
             var color = spriteRenderer.color;
@@ -89,14 +125,11 @@ namespace Mouse
         {
             return spriteRenderer.color.a;
         }
-        
+
         private IEnumerator StartCoroutineWithDelay(float delay, IEnumerator targetCoroutine)
         {
             yield return new WaitForSeconds(delay);
             StartCoroutine(targetCoroutine);
         }
-
-
-
     }
 }
