4d673b7233cf1b33079dfe51ab4387135e99f716
Martin <martini@jeniffer-2.local>
2024-08-15 01:04:18 +0800

    chore: removed uneeded code

--- Assets/Scripts/Projectiles/SlownessProjectile.cs
+++ Assets/Scripts/Projectiles/SlownessProjectile.cs
@@ -6,13 +6,12 @@ public class SlownessProjectile : MonoBehaviour
     [SerializeField] public float slownessFactor = 5; //how much the mouse will be slowed down by
     [SerializeField] public float duration = 3; //how long the mouse will be slowed down for
     public float projectileSpeed; // speed of projectile
-    public bool hit = false;
     [SerializeField] private float destroyTime; // time until projectile is destroyed (seconds)
     [SerializeField] private GameObject soup;
 
     void Update()
     {
-        if (!hit) transform.position += transform.up * Time.deltaTime * projectileSpeed;      // update position
+        transform.position += transform.up * Time.deltaTime * projectileSpeed;      // update position
 
         // destroy game object when timer reaches zero
         destroyTime -= Time.deltaTime;
@@ -27,12 +26,9 @@ public class SlownessProjectile : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Mouse"))
         {
-            hit = true;
             if (transform.name.Equals("Projectile Potager 4(Clone)"))
             {
-                //StartCoroutine(GoopDelay(duration, collision));
                 GameObject gooper = Instantiate(soup, collision.transform.position, collision.transform.rotation);
-                Debug.Log(gooper.name, gooper);
                 Destroy(gooper, duration);
             }
             Destroy(gameObject);
