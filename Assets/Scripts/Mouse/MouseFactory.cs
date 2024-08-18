297340a71e258fa626e0d6294b389d564b8c5179
antoshNikolak <antoninikolak@gmail.com>
2024-08-18 22:09:01 +0100

    removed print statement

--- Assets/Scripts/Mouse/MouseFactory.cs
+++ Assets/Scripts/Mouse/MouseFactory.cs
@@ -34,7 +34,6 @@ namespace Mouse
         public void SpawnMouse(MiceScriptableObject mouseType, Vector3 position, int wayPointIndex, float pathDistance)
         {
             GameObject newMouse = Instantiate(mousePrefab, position, transform.rotation); // Instantiate mouse prefab
-            print("NEW MOUSE: " + newMouse);
             newMouse.GetComponent<MouseStats>().loadStats(mouseType);
             var spriteMove = newMouse.GetComponent<SpriteMove>();
             spriteMove.SetTargetWayPointIndex(wayPointIndex);
