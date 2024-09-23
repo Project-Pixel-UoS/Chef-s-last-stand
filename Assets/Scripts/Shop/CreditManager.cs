8714f76c8760362ed6f9ed56a4f5082848bf0c18
Emzlovesrobots <118927917+Emzlovesrobots@users.noreply.github.com>
2024-09-23 22:35:08 +0100

    inital changes

--- Assets/Scripts/Shop/CreditManager.cs
+++ Assets/Scripts/Shop/CreditManager.cs
@@ -13,7 +13,7 @@ public class CreditManager : MonoBehaviour
 
     void Start()
     {
-        credits = 4000; // should be 40 in production mode
+        credits = 40; // should be 40 in production mode
         text.text = "Credits: " + credits;
     }
 
