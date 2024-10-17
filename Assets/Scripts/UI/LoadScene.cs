18ef7046614078bbd3cb29d72a23690feeb44959
antoshNikolak <antoninikolak@gmail.com>
2024-10-17 17:10:26 +0100

    [fix] added back scene loader code

--- Assets/Scripts/UI/LoadScene.cs
+++ Assets/Scripts/UI/LoadScene.cs
@@ -3,7 +3,13 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
+    
+    /// <summary>
+    /// This method is referenced by different buttons
+    /// </summary>
+    /// <param name="sceneName"></param>
     public void LoadScene(string sceneName)
     {
+        SceneManager.LoadScene(sceneName);
     }
 }
